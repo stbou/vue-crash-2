4a039cf5bd35dd43dfbf73f1fa70a4a725c60e7d
Stephane Boulanger <stephboul22@gmail.com>
2022-10-17 22:01:43 -0400

    fix the delete cross sign

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -4,7 +4,7 @@
         :class="[task.reminder ? 'reminder' : '', 'task']">
           <h3>
             {{ task.text }}
-            <i @click="$emit('delete-task', task.id)" class="fas fa-times">X</i>   
+            <i @click="$emit('delete-task', task.id)" class="fas fa-times"></i>   
           </h3>
           <p>{{ task.day }}</p>
     </div>
